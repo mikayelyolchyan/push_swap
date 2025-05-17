cdcfdf24358455d18b8b5d661f89b8d08b9e6061
nosoftplayer <nosoftplayer@kali>
2025-05-17 18:54:35 +0400

    structuring and adding Makefile

---
+++ includes/headers/push_swap.h
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: miyolchy <miyolchy@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/17 18:43:49 by miyolchy          #+#    #+#             */
+/*   Updated: 2025/05/17 18:45:06 by miyolchy         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include "../libraries/libft/libft.h"
+# include "../libraries/ft_printf/ft_printf.h"
+
+#endif
